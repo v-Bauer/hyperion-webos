9fdce8fbab2170277c5a25f070498a0d86c20f36
Vadim Bauer <vadim.bauer@ard.de>
2022-02-01 15:19:42 +0100

    Changed to usleep

--- src/backends/libdile_vt.c
+++ src/backends/libdile_vt.c
@@ -179,7 +179,7 @@ int capture_start()
 
     // Prepare offsets table (needs to be ptr[vfbcap.numVfs][vbcap.numPlanes])
     if (DILE_VT_GetVideoFrameBufferCapability(vth, &vfbcap) != 0) {
-        return -9;
+        usleep(30000);
     }
 
     INFO("[DILE_VT] vfbs: %d; planes: %d", vfbcap.numVfbs, vfbcap.numPlanes);
